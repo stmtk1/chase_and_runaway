478951b8367580b5684ef1d7d52520a68fc7649d
stmtk1 <13043069@gakushuin.ac.jp>
2018-11-27 20:14:08 +0900

    finished piston introdution

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,15 @@
-extern crate piston_window;
 extern crate rand;
+extern crate piston;
+extern crate glutin_window;
+extern crate opengl_graphics;
+extern crate graphics;
 
 use rand::prelude::*;
-use piston_window::*;
-
-fn main(){
-    for _ in 0..100{
-        let width: f64 = 100.0;
-        let height: f64 = 100.0;
-        let cat = Cat::new(width, height);
-        let rat = Rat::new(width, height);
-        println!("{}", Animal::dist(cat.as_animal(), rat.as_animal()));
-        World::new(width, height);
-    }
-}
+use piston::window::WindowSettings;
+use piston::event_loop::*;
+use piston::input::*;
+use glutin_window::GlutinWindow as Window;
+use opengl_graphics::{ GlGraphics, OpenGL };
 
 struct Animal {
     x: f64,
@@ -70,32 +66,64 @@ impl AnimalTrait for Rat {
     }
 }
 
-struct World{
-    window: PistonWindow,
-    cats: Vec<Cat>,
-    rats: Vec<Rat>,
-    height: f64,
-    width: f64
+pub struct App {
+    gl: GlGraphics,
+    rotation: f64
 }
 
-impl World {
-    fn new(width: f64, height: f64) -> World {
-        World {
-            window: World::create_window(width, height),
-            cats: vec![],
-            rats: vec![],
-            height: height,
-            width: width
-        }
+impl App {
+    fn render(&mut self, args: &RenderArgs){
+        use graphics::*;
+        
+        const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
+        const RED:   [f32; 4] = [1.0, 0.0, 0.0, 1.0];
+
+        let square = rectangle::square(0.0, 0.0, 50.0);
+        let rotation = self.rotation;
+        let (x, y) = (args.width / 2.0, args.height / 2.0);
+
+        self.gl.draw(args.viewport(), |c, gl|{
+            clear(GREEN, gl);
+            
+            let transform = 
+                c.transform
+                .trans(x, y)
+                .rot_rad(rotation)
+                .trans(-25.0, -25.0);
+            rectangle(RED, square, transform, gl);
+        });
     }
 
-    fn create_window(width: f64, height: f64) -> PistonWindow {
-        WindowSettings::new("chase and run away", [width as u32, height as u32])
-            .exit_on_esc(true)
-            .build()
-            .unwrap()
+    fn update(&mut self, args: &UpdateArgs) {
+        self.rotation += 2.0 * args.dt;
     }
+}
+
+fn main(){
+    let opengl = OpenGL::V3_2;
+    let mut window: Window = WindowSettings::new(
+        "spinning-square",
+        [200, 200]
+        )
+        .opengl(opengl)
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
     
-    fn clear_window(window: PistonWindow){
+    let mut app = App {
+        gl: GlGraphics::new(opengl),
+        rotation: 0.0
+    };
+    
+    let mut events = Events::new(EventSettings::new());
+    
+    while let Some(e) = events.next(&mut window) {
+        if let Some(r) = e.render_args(){ 
+            app.render(&r);
+        }
+        
+        if let Some(u) = e.update_args() {
+            app.update(&u);
+        }
     }
 }
