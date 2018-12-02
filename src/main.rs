18d197aa0f7d0724b13b92732d3fc06e39defa82
motoki saito <13043069@gakushuin.ac.jp>
2018-12-02 23:42:40 +0900

    deleted Cat and Rat struct

--- src/main.rs
+++ src/main.rs
@@ -23,101 +23,56 @@ struct Animal {
     direction: f64,
 }
 
-#[derive(Clone)]
-struct Cat{
-    x: f64,
-    y: f64,
-    velocity: f64,
-    direction: f64,
-}
-
-#[derive(Clone)]
-struct Rat{
-    x: f64,
-    y: f64,
-    velocity: f64,
-    direction: f64,
-}
-
-trait AnimalTrait {
-    fn as_animal(&self) -> Animal;
-}
-
 impl Animal{
+    fn new() -> Animal{
+        let mut rng = rand::thread_rng();
+        let x: f64 = rng.gen::<f64>() * WIDTH;
+        let y: f64 = rng.gen::<f64>() * HEIGHT;
+        let theta: f64 = rng.gen::<f64>() * 2.0 * (std::f64::consts::PI);
+        Animal{ x: x, y: y, velocity: 0.5, direction: theta }
+    }
+    
     fn dist(a1: Animal, a2: Animal) -> f64 {
         (a1.x - a2.x) * (a1.x - a2.x) + (a1.y - a2.y) * (a1.y - a2.y)
     }
     
-    fn move_self(&mut self) {
-        self.x += self.velocity * self.direction.cos();
-        self.y += self.velocity * self.direction.sin();
+    fn move_self(&self) -> Animal {
+        let mut new_x = self.x + self.velocity * self.direction.cos();
+        let mut new_y = self.y + self.velocity * self.direction.sin();
         
-        if self.x > WIDTH {
-            self.x -= WIDTH;
+        if new_x > WIDTH {
+            new_x -= WIDTH;
         }
         
-        if self.x < 0.0 {
-            self.x += WIDTH;
+        if new_x < 0.0 {
+            new_x += WIDTH;
         }
         
-        if self.y > HEIGHT {
-            self.y -= HEIGHT;
+        if new_y > HEIGHT {
+            new_y -= HEIGHT;
         }
         
-        if self.y < 0.0 {
-            self.y += HEIGHT;
+        if new_y < 0.0 {
+            new_y += HEIGHT;
         }
-    }
-    
-    fn as_cat(&self) -> Cat {
-        Cat{x: self.x, y: self.y, velocity: self.velocity, direction: self.direction }
-    }
-    
-    fn as_rat(&self) -> Rat {
-        Rat{x: self.x, y: self.y, velocity: self.velocity, direction: self.direction }
+        
+        Animal {
+            x: new_x,
+            y: new_y,
+            velocity: self.velocity,
+            direction: self.direction
+        }
     }
     
     fn find_near(&self, others: Vec<Animal>){
     }
 }
 
-impl Cat{
-    fn new() -> Cat{
-        let mut rng = rand::thread_rng();
-        let x: f64 = rng.gen::<f64>() * WIDTH;
-        let y: f64 = rng.gen::<f64>() * HEIGHT;
-        let theta: f64 = rng.gen::<f64>() * 2.0 * (std::f64::consts::PI);
-        Cat{ x: x, y: y, velocity: 0.5, direction: theta }
-    }
-}
-
-impl Rat{
-    fn new() -> Rat {
-        let mut rng = rand::thread_rng();
-        let x: f64 = rng.gen::<f64>() * WIDTH;
-        let y: f64 = rng.gen::<f64>() * HEIGHT;
-        let theta: f64 = rng.gen::<f64>() * 2.0 * (std::f64::consts::PI);
-        Rat{ x: x, y: y, velocity: 0.5, direction: theta }
-    }
-}
-
-impl AnimalTrait for Cat {
-    fn as_animal(&self) -> Animal {
-        Animal {x: self.x, y: self.y, velocity: self.velocity, direction: self.direction }
-    }
-}
-
-impl AnimalTrait for Rat {
-    fn as_animal(&self) -> Animal {
-        Animal {x: self.x, y: self.y, velocity: self.velocity, direction: self.direction }
-    }
-}
-
 // #[derive(Clone)]
 pub struct App {
     gl: GlGraphics,
-    cats: Vec<Cat>,
-    rats: Vec<Rat>,
+    cats: Vec<Animal>,
+    rats: Vec<Animal>,
 }
 
 impl App {
@@ -154,20 +109,16 @@ impl App {
 
     fn update(&mut self) {
         let cats = &self.cats.clone();
-        let mut new_cats: Vec<Cat> = Vec::with_capacity(cats.len());
+        let mut new_cats: Vec<Animal> = Vec::with_capacity(cats.len());
         for cat in cats {
-            let mut animal = cat.as_animal();
-            animal.move_self();
-            new_cats.push(animal.as_cat());
+            new_cats.push(cat.move_self());
         }
         self.cats = new_cats;
         
         let rats = &self.rats.clone();
-        let mut new_rats: Vec<Rat> = Vec::with_capacity(rats.len());
+        let mut new_rats: Vec<Animal> = Vec::with_capacity(rats.len());
         for rat in rats {
-            let mut animal = rat.as_animal();
-            animal.move_self();
-            new_rats.push(animal.as_rat());
+            new_rats.push(rat.move_self());
         }
         self.rats = new_rats;
     }
@@ -184,14 +135,14 @@ fn main(){
         .build()
         .unwrap();
     
-    let mut cats: Vec<Cat> = Vec::with_capacity(100);
+    let mut cats: Vec<Animal> = Vec::with_capacity(100);
     for _ in 0..100 {
-        cats.push(Cat::new());
+        cats.push(Animal::new());
     }
     
-    let mut rats: Vec<Rat> = Vec::with_capacity(100);
+    let mut rats: Vec<Animal> = Vec::with_capacity(100);
     for _ in 0..100 {
-        rats.push(Rat::new());
+        rats.push(Animal::new());
     }
     
     let mut app = App {
