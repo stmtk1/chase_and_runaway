a94cc6a970aa757d256c979bf32c4fb6e05b7c68
斎藤基樹 <stmtk13044032@gmail.com>
2019-02-26 19:30:31 +0900

    consts追加

--- src/consts.rs
+++ src/consts.rs
@@ -1,2 +1,17 @@
 pub const WIDTH: f64 = 640.0;
 pub const HEIGHT: f64 = 480.0;
+pub const ENERGY_MAX: u64 = 1000;
+pub const RAT_VELOCITY: f64 = 0.5;
+pub const CAT_VELOCITY: f64 = 1.0;
+pub const MUTATE_ABS: f64 = 10.0;
+pub const RUNAWAY_RADIOUS: f64 = 10.0;
+pub const COHENSION_RADIOUS: f64 = 15.0;
+pub const EATEN_RADIOUS: f64 = 1.0;
+pub const ALIGN_RADIOUS: f64 = 10.0;
+pub const SEPARATE_RADIOUS: f64 = 5.0;
+pub const CHASE_RADIOUS: f64 = 10.0;
+pub const EAT_ENERGY: u64 = 300;
+pub const COHENSION_MAX: f64 = 480.0;
+pub const CHASE_MAX: f64 = 480.0;
+pub const SEPARATE_MAX: f64 = 480.0;
+pub const ALIGN_MAX: f64 = 480.0;
